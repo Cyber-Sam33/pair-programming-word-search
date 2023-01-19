786d6c581ded96c7905fa4ca108bdee6bf70a163
Cyber-Sam33 <issasammi@yahoo.com>
2023-01-19 03:31:49 +0000

    Initial commit

--- test/test_wordsearch.js
+++ test/test_wordsearch.js
@@ -1,7 +1,7 @@
 const chai = require('chai');
 const assert = chai.assert;
 
-const wordSearch = require('../wordsearch.js')
+const wordSearch = require('../wordsearch.js');
 
 describe("#wordSearch()", function() {
   it("should return false if the word is not present", function() {
@@ -15,12 +15,12 @@ describe("#wordSearch()", function() {
       ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
       ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
       ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
-    ], 'FRANK')
+    ], 'FRANK');
 
     assert.isFalse(result);
   });
 
-  it("should return true if the word is present", function() {
+  it("should return true if the word is present horizontally", function() {
     const result = wordSearch([
       ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
       ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
@@ -31,7 +31,55 @@ describe("#wordSearch()", function() {
       ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
       ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
       ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
-    ], 'SEINFELD')
+    ], 'SEINFELD');
+
+    assert.isTrue(result);
+  });
+
+  it("should return true if the word is present vertically (array transposed)", function() {
+
+    const result = wordSearch([
+      [
+        'A', 'S', 'Y',
+        'H', 'W', 'B',
+        'U', 'O', 'E'
+      ],
+      [
+        'W', 'E', 'F',
+        'M', 'H', 'F',
+        'B', 'D', 'Z'
+      ],
+      [
+        'C', 'I', 'C',
+        'J', 'C', 'R',
+        'T', 'C', 'K'
+      ],
+      [
+        'F', 'N', 'F',
+        'T', 'S', 'E',
+        'W', 'A', 'F'
+      ],
+      [
+        'Q', 'F', 'Q',
+        'E', 'Y', 'N',
+        'A', 'K', 'Q'
+      ],
+      [
+        'U', 'E', 'U',
+        'V', 'E', 'E',
+        'P', 'U', 'U'
+      ],
+      [
+        'A', 'L', 'A',
+        'R', 'R', 'Y',
+        'A', 'A', 'A'
+      ],
+      [
+        'L', 'D', 'L',
+        'G', 'L', 'B',
+        'I', 'S', 'L'
+      ]
+    ], 'VEEP');
 
     assert.isTrue(result);
   });
